0a5739296aa9dc91dacd03ffacc4a1e92658b7f6
Dilan Patel <dilanpace1496@gmail.com>
2019-03-12 12:05:32 -0400

    hannah's garbo + my tech support garbo = die a beet us

--- scene.py
+++ scene.py
@@ -4,6 +4,12 @@ import random
 
 user_rating = int(input("How many stars do you want yeeted?: "))
 
+numofbuildings = 40
+buildingstarty = -200
+
+chad.speed(0)
+user_buildings = int(input("How many buildings do you want? MAX 20: "))
+
 chad.speed(0)
 
 def draw_space():
@@ -66,7 +72,7 @@ def stars():
 #Draw stars
 def draw_stars():
     for x in range(user_rating):
-        chad.setposition(random.randint(-500,500),random.randint(100,250))
+        chad.setposition(random.randint(-400,400),random.randint(100,250))
         chad.pendown()
         stars()
         chad.penup()
@@ -109,8 +115,7 @@ def crater():
 for i in range(1):
     chad.penup()
     chad.setposition(-500,-250)
-    #hungry = random.randint(0,1)
-    hungry = 0
+    hungry = random.randint(0,1)
     chad.pendown()
     if(hungry == 0):
         chad.color("#66ccff")
@@ -143,3 +148,83 @@ for i in range(1):
         draw_earth()
     border()
       
+    chad.left(180)
+def building(buildingnum, startY, sequence):
+    chad.pensize(2)
+    buildingheight = random.randint(100,250)
+    buildingwidth = random.randint(50,75)
+    buildY = (startY+((25 / buildingnum) * sequence))
+    chad.setpos(random.randint(-400,400), buildY)
+    chad.color("grey")
+    chad.begin_fill()
+    chad.pendown()
+    chad.forward(buildingwidth)
+    chad.left(90)
+    chad.forward(buildingheight)
+    chad.left(90)
+    chad.forward(buildingwidth)
+    chad.left(90)
+    chad.forward(buildingheight)
+    chad.left(90)
+    chad.end_fill()
+    chad.penup()
+    
+    chad.forward((buildingwidth*1/3)-5)
+    chad.left(90)
+    chad.forward(20)
+    chad.left(-90)
+    chad.pendown()
+    for i in range(10, buildingheight, 20):
+        chad.color("yellow")
+        chad.pendown()
+        chad.begin_fill()
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.end_fill()
+        chad.penup()
+        chad.left(90)
+        chad.forward(15)
+        chad.left(-90)
+    for i in range(10, buildingheight, 20):
+        chad.penup()
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.left(90)
+        chad.forward(-15)
+        chad.left(-90)
+    chad.forward((buildingwidth*1/3))
+    chad.pendown()
+    for i in range(10, buildingheight, 20):
+        chad.color("yellow")
+        chad.pendown()
+        chad.begin_fill()
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.forward(10)
+        chad.left(90)
+        chad.end_fill()
+        chad.penup()
+        chad.left(90)
+        chad.forward(15)
+        chad.left(-90)
+
+chad.penup()
+if (user_buildings <= 20):
+    for i in range(user_buildings+1, 1, -1):
+        building(user_buildings+1, buildingstarty, i)
